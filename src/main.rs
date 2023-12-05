476bb9e280308f555a222db8cc923b1d9d7720ee
Clancy Kornie <91301915+ckornie@users.noreply.github.com>
2023-12-06 10:24:15 +1100

    Read in file for first solution

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::Error;
+
+fn main() -> Result<(), Error> {
+    let data = include_str!("../resources/2023-12-01-001.txt");
+    for line in data.split('\n') {
+        println!("{}", line);
+    }
+
+    Ok(())
 }
