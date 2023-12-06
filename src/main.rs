667d14af2c5acf6b15c976a3ef4eed618c036651
Clancy Kornie <91301915+ckornie@users.noreply.github.com>
2023-12-06 13:09:15 +1100

    Solution to first problem
    This is a very simplistic solution to the first problem.

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,30 @@
 use std::io::Error;
 
 fn main() -> Result<(), Error> {
-    let data = include_str!("../resources/2023-12-01-001.txt");
-    for line in data.split('\n') {
-        println!("{}", line);
+    let input = include_str!("../resources/2023-12-01-001.txt");
+    let mut line: Vec<char> = Vec::new();
+    let mut values: Vec<u32> = Vec::new();
+
+    for c in input.chars() {
+        if c.is_digit(10) {
+            line.push(c);
+        } else if c == '\n' {
+            if line.len() >= 1 {
+                let first = line.first().unwrap();
+                let second = line.last().unwrap();
+                let mut number = first.to_string();
+                number.push(*second);
+                values.push(number.parse::<u32>().unwrap());
+            } else {
+                eprintln!("Did not find any digits.");
+            }
+
+            line.clear();
+        }
     }
+
+    let sum: u32 = values.iter().sum();
+    println!("The total sum is: {}", sum);
 
     Ok(())
 }
