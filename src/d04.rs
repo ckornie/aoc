9008f39a2f5cb5a05cf291a2e3ccd21b1e299bff
Clancy Kornie <91301915+ckornie@users.noreply.github.com>
2023-12-15 20:56:42 +1100

    Solution for day 4 part 1

--- src/d04.rs
+++ src/d04.rs
@@ -1,9 +1,61 @@
+use std::collections::HashSet;
+
 use anyhow::{anyhow, bail, Context, Error, Result};
 use itertools::Itertools;
 
 #[derive(Debug)]
-pub struct Part {
-    number: u32,
+pub struct Card {
+    id: u32,
+    wins: HashSet<u32>,
+    ours: HashSet<u32>,
+}
+
+impl Card {
+    fn points(&self) -> u32 {
+        let intersection: Vec<&u32> = self.wins.intersection(&self.ours).collect();
+        if intersection.len() > 0 {
+            1 << intersection.len() - 1
+        } else {
+            0
+        }
+    }
+}
+
+fn parse_numbers(data: &str) -> Result<HashSet<u32>> {
+    data.split(' ')
+        .into_iter()
+        .map(|number| number.trim())
+        .filter(|number| !number.is_empty())
+        .map(|number| {
+            number
+                .parse::<u32>()
+                .with_context(|| format!("couldn't parse number {:?}", number))
+        })
+        .collect()
+}
+
+fn parse_cards(data: &str) -> Result<Vec<Card>> {
+    data.split('\n')
+        .filter(|card| !card.is_empty())
+        .map(|card| match card.split_once(':') {
+            Some((id, numbers)) => match numbers.split_once('|') {
+                Some((wins, ours)) => Ok(Card {
+                    id: id["Card ".len()..].trim().parse::<u32>()?,
+                    wins: parse_numbers(wins)?,
+                    ours: parse_numbers(ours)?,
+                }),
+                None => bail!("couldn't parse numbers"),
+            },
+            None => bail!("couldn't parse card"),
+        })
+        .collect()
+}
+
+pub fn guessed_points(data: &str) -> Result<u32> {
+    Ok(parse_cards(data)?
+        .iter()
+        .map(|card| card.points())
+        .sum::<u32>())
 }
 
 #[cfg(test)]
@@ -13,14 +65,14 @@ mod tests {
     #[test]
     fn part_1_example() -> Result<()> {
         let input = include_str!("../res/04.example");
-        assert_eq!(1, 1);
+        assert_eq!(guessed_points(input)?, 13);
         Ok(())
     }
 
     #[test]
     fn part_1_actual() -> Result<()> {
         let input = include_str!("../res/04.actual");
-        assert_eq!(1, 1);
+        assert_eq!(guessed_points(input)?, 13);
         Ok(())
     }
 
