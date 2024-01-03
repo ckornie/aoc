b727dbf122cc93b381e18661d5566f5759e92c1b
Clancy Kornie <91301915+ckornie@users.noreply.github.com>
2024-01-03 21:47:43 +1100

    Solution for day 5 part 2

--- src/d05.rs
+++ src/d05.rs
@@ -1,4 +1,4 @@
-use anyhow::{bail, Context, Result};
+use anyhow::{Context, Result};
 use itertools::Itertools;
 
 #[derive(Debug)]
@@ -8,10 +8,24 @@ pub struct Bijection {
     offset: i64,
 }
 
+#[derive(Debug)]
+pub struct Location {
+    result: i64,
+    supremum: i64,
+}
+
 impl Bijection {
-    fn location(&self, seed: i64) -> Option<i64> {
+    fn location(&self, seed: i64) -> Option<Location> {
         if self.start <= seed && self.end > seed {
-            Some(seed + self.offset)
+            Some(Location {
+                result: seed + self.offset,
+                supremum: self.end - seed,
+            })
+        } else if self.start > seed {
+            Some(Location {
+                result: seed,
+                supremum: self.start - seed,
+            })
         } else {
             None
         }
@@ -21,7 +35,7 @@ impl Bijection {
 #[derive(Debug)]
 pub struct Data {
     seeds: Vec<i64>,
-    bijections: Vec<Vec<Bijection>>,
+    mapping: Vec<Vec<Bijection>>,
 }
 
 impl TryFrom<&str> for Data {
@@ -35,14 +49,15 @@ impl TryFrom<&str> for Data {
             .map(|seed| seed.parse::<i64>().with_context(|| "cannot parse seeds"))
             .collect::<Result<Vec<i64>>>()?;
 
-        let mut bijections: Vec<Vec<Bijection>> = vec![];
+        let mut mapping: Vec<Vec<Bijection>> = vec![];
         let mut map: Vec<Bijection> = vec![];
 
         for i in 1..lines.len() {
             let line = lines[i];
             if !line.is_empty() {
                 if line.ends_with(" map:") && !map.is_empty() {
-                    bijections.push(map);
+                    map.sort_by(|a, b| a.start.cmp(&b.start));
+                    mapping.push(map);
                     map = vec![];
                 } else if let Some((Ok(codomain), Ok(domain), Ok(span))) = line
                     .split(' ')
@@ -59,75 +74,53 @@ impl TryFrom<&str> for Data {
         }
 
         map.sort_by(|a, b| a.start.cmp(&b.start));
-        bijections.push(map);
+        mapping.push(map);
 
-        Ok(Data { seeds, bijections })
+        Ok(Data { seeds, mapping })
     }
 }
 
-struct PartOne {
-    data: Data,
-}
-
-impl PartOne {
-    fn lowest(&self) -> i64 {
-        let mut lowest = i64::MAX;
-        for &seed in &self.data.seeds {
-            let mut seed = seed;
-            for bijections in &self.data.bijections {
-                for maps in bijections {
-                    if let Some(location) = maps.location(seed) {
-                        seed = location;
-                        break;
-                    }
-                }
-            }
-            lowest = if lowest < seed { lowest } else { seed }
-        }
-
-        lowest
-    }
-}
-
-struct PartTwo {
-    data: Data,
-}
-
-impl PartTwo {
-    fn seeds(&self) -> Vec<i64> {
-        self.data
-            .seeds
-            .iter()
-            .tuples::<(&i64, &i64)>()
-            .map(|(&start, &length)| {
-                let mut seeds: Vec<i64> = vec![];
-                for i in start..start + length {
-                    seeds.push(i);
-                }
-                seeds
+pub fn part_one(data: Data) -> Option<i64> {
+    data.seeds
+        .iter()
+        .map(|&seed| {
+            data.mapping.iter().fold(seed, |seed, map| {
+                map.iter()
+                    .filter_map(|bijection| bijection.location(seed))
+                    .min_by(|a, b| a.supremum.cmp(&b.supremum))
+                    .map_or(seed, |location| location.result)
             })
-            .flatten()
-            .collect()
-    }
-
-    fn lowest(&self) -> i64 {
-        let mut lowest = i64::MAX;
-        for seed in self.seeds() {
-            let mut seed = seed;
-            for bijections in &self.data.bijections {
-                for maps in bijections {
-                    if let Some(location) = maps.location(seed) {
-                        seed = location;
-                        break;
-                    }
-                }
-            }
-            lowest = if lowest < seed { lowest } else { seed }
-        }
-
-        lowest
-    }
+        })
+        .min()
 }
+
+pub fn part_two(data: Data) -> Option<i64> {
+    data.seeds
+        .iter()
+        .tuples::<(&i64, &i64)>()
+        .flat_map(|(&start, &length)| {
+            let mut results = vec![];
+            let mut seed = start;
+            while seed < (start + length) {
+                let (result, supremum) =
+                    data.mapping
+                        .iter()
+                        .fold((seed, i64::MAX), |(seed, supremum), map| {
+                            map.iter()
+                                .filter_map(|bijection| bijection.location(seed))
+                                .min_by(|a, b| a.supremum.cmp(&b.supremum))
+                                .map_or((seed, supremum), |location| {
+                                    (location.result, supremum.min(location.supremum))
+                                })
+                        });
+                results.push(result);
+                seed = seed + supremum;
+            }
+            results
+        })
+        .min()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -135,40 +128,28 @@ mod tests {
     #[test]
     fn part_1_example() -> Result<()> {
         let input = include_str!("../res/05.example");
-        let almanac = PartOne {
-            data: Data::try_from(input)?,
-        };
-        assert_eq!(almanac.lowest(), 35);
+        assert_eq!(part_one(Data::try_from(input)?), Some(35));
         Ok(())
     }
 
     #[test]
     fn part_1_actual() -> Result<()> {
         let input = include_str!("../res/05.actual");
-        let almanac = PartOne {
-            data: Data::try_from(input)?,
-        };
-        assert_eq!(almanac.lowest(), 486_613_012);
+        assert_eq!(part_one(Data::try_from(input)?), Some(486_613_012));
         Ok(())
     }
 
     #[test]
     fn part_2_example() -> Result<()> {
         let input = include_str!("../res/05.example");
-        let almanac = PartTwo {
-            data: Data::try_from(input)?,
-        };
-        assert_eq!(almanac.lowest(), 46);
+        assert_eq!(part_two(Data::try_from(input)?), Some(46));
         Ok(())
     }
 
     #[test]
     fn part_2_actual() -> Result<()> {
         let input = include_str!("../res/05.actual");
-        let almanac = PartTwo {
-            data: Data::try_from(input)?,
-        };
-        assert_eq!(almanac.lowest(), 46);
+        assert_eq!(part_two(Data::try_from(input)?), Some(56931769));
         Ok(())
     }
 }
