aceb314ee8fa262c707a5b48bdb058d000b55c99
Clancy Kornie <91301915+ckornie@users.noreply.github.com>
2024-01-17 07:47:23 +1100

    Solution for day 9 part 2

--- src/d09.rs
+++ src/d09.rs
@@ -1,6 +1,5 @@
-use anyhow::{anyhow, bail, Context, Result};
+use anyhow::{Context, Result};
 use itertools::Itertools;
-use std::collections::HashMap;
 
 fn readings(input: &str) -> Result<Vec<Vec<i64>>> {
     input
@@ -14,31 +13,50 @@ fn readings(input: &str) -> Result<Vec<Vec<i64>>> {
         .with_context(|| "unexpected format")
 }
 
-fn predict(history: &Vec<i64>) -> i64 {
-    if history.iter().all_equal() {
-        return history.last().map(|&e| e).unwrap_or_default();
+fn predict(readings: &Vec<i64>) -> i64 {
+    if readings.iter().all_equal() {
+        return readings.last().map(|&e| e).unwrap_or_default();
     }
 
     let next = predict(
-        &history
+        &readings
             .iter()
             .tuple_windows()
             .map(|(&a, &b)| b - a)
             .collect(),
     );
 
-    history.last().map(|&e| e + next).unwrap_or_default()
+    readings.last().map(|&e| e + next).unwrap_or_default()
+}
+
+fn extrapolate(readings: &Vec<i64>) -> i64 {
+    if readings.iter().all(|&reading| reading == 0) {
+        return readings.first().map(|&e| e).unwrap_or_default();
+    }
+
+    let next = extrapolate(
+        &readings
+            .iter()
+            .tuple_windows()
+            .map(|(&a, &b)| b - a)
+            .collect(),
+    );
+
+    readings.first().map(|&e| e - next).unwrap_or_default()
 }
 
 pub fn part_one(input: &str) -> Result<i64> {
     Ok(readings(input)?
         .iter()
-        .map(|history| predict(history))
+        .map(|readings| predict(readings))
         .sum())
 }
 
-pub fn part_two(input: &str) -> Result<usize> {
-    Ok(0)
+pub fn part_two(input: &str) -> Result<i64> {
+    Ok(readings(input)?
+        .iter()
+        .map(|readings| extrapolate(readings))
+        .sum())
 }
 
 #[cfg(test)]
@@ -63,12 +81,14 @@ mod tests {
     #[test]
     fn part_2_example() -> Result<()> {
         let input = include_str!("../res/09.example");
+        assert_eq!(part_two(input)?, 2);
         Ok(())
     }
 
     #[test]
     fn part_2_actual() -> Result<()> {
         let input = include_str!("../res/09.actual");
+        assert_eq!(part_two(input)?, 1050);
         Ok(())
     }
 }
