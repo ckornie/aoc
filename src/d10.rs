8fe9d7f5ce7578d8f5fa08322adf4ef0cc74380d
Clancy Kornie <91301915+ckornie@users.noreply.github.com>
2024-01-22 07:59:59 +1100

    Solution for day 10 part 2

--- src/d10.rs
+++ src/d10.rs
@@ -1,5 +1,4 @@
-use anyhow::{bail, Context, Result};
-use itertools::Itertools;
+use anyhow::{bail, Result};
 
 #[derive(Debug, Clone, Copy)]
 pub enum Direction {
@@ -9,9 +8,35 @@ pub enum Direction {
     L = 0b1000,
 }
 
+impl Direction {
+    fn next(self) -> Direction {
+        match self {
+            Direction::T => Direction::R,
+            Direction::R => Direction::B,
+            Direction::B => Direction::L,
+            Direction::L => Direction::T,
+        }
+    }
+
+    fn previous(self) -> Direction {
+        self.next().next().next()
+    }
+
+    fn pipe(self, direction: Direction) -> Option<Pipe> {
+        match (self, direction) {
+            (Direction::T, Direction::B) => Some(Pipe::NS),
+            (Direction::T, Direction::R) => Some(Pipe::NE),
+            (Direction::T, Direction::L) => Some(Pipe::NW),
+            (Direction::R, Direction::B) => Some(Pipe::SE),
+            (Direction::R, Direction::L) => Some(Pipe::EW),
+            (Direction::L, Direction::B) => Some(Pipe::SW),
+            _ => None,
+        }
+    }
+}
+
 #[derive(Debug, Clone, Copy, Eq, PartialEq)]
-pub enum Tile {
-    //     LBRTLBRT
+pub enum Pipe {
     BL = 0b00000000,
     ST = 0b11111111,
     NS = 0b01010101,
@@ -22,112 +47,175 @@ pub enum Tile {
     SW = 0b00111100,
 }
 
-impl Tile {
-    fn connected(self, neighbour: Tile, direction: Direction) -> bool {
+impl Pipe {
+    fn connected(self, neighbour: Pipe, direction: Direction) -> bool {
         ((direction as u32 & self as u32) << 4) & neighbour as u32 > 0
     }
+
+    fn northbound(self) -> bool {
+        Direction::T as u32 & self as u32 > 0
+    }
 }
 
-impl From<char> for Tile {
+impl From<char> for Pipe {
     fn from(value: char) -> Self {
         match value {
-            '|' => Tile::NS,
-            '-' => Tile::EW,
-            'L' => Tile::NE,
-            'J' => Tile::NW,
-            '7' => Tile::SW,
-            'F' => Tile::SE,
-            'S' => Tile::ST,
-            _ => Tile::BL,
+            '|' => Pipe::NS,
+            '-' => Pipe::EW,
+            'L' => Pipe::NE,
+            'J' => Pipe::NW,
+            '7' => Pipe::SW,
+            'F' => Pipe::SE,
+            'S' => Pipe::ST,
+            _ => Pipe::BL,
         }
     }
 }
 
-struct Network {
-    tiles: Vec<Vec<Tile>>,
+#[derive(Debug, Clone, Copy)]
+struct Tile {
     row: usize,
     column: usize,
+    pipe: Pipe,
+    origin: bool,
+    connected: bool,
 }
 
-impl From<&str> for Network {
-    fn from(value: &str) -> Self {
-        let mut tiles: Vec<Vec<Tile>> = vec![];
-        let mut row = 0;
-        let mut column = 0;
-        for (i, line) in value.lines().enumerate() {
-            let mut pending = vec![];
-            for (j, character) in line.chars().enumerate() {
-                let tile = Tile::from(character);
-                if tile == Tile::ST {
-                    row = i;
-                    column = j;
-                }
-                pending.push(tile);
-            }
-            tiles.push(pending);
+impl Tile {
+    fn new(row: usize, column: usize, pipe: char) -> Tile {
+        let pipe = Pipe::from(pipe);
+        Tile {
+            row,
+            column,
+            pipe,
+            origin: pipe == Pipe::ST,
+            connected: pipe == Pipe::ST,
         }
+    }
 
-        Network { tiles, row, column }
+    fn connect(self) -> Tile {
+        Tile {
+            connected: true,
+            ..self
+        }
+    }
+
+    fn link(self, first: Direction, second: Direction) -> Tile {
+        let pipe = first
+            .pipe(second)
+            .or(second.pipe(first))
+            .unwrap_or(Pipe::BL);
+
+        Tile { pipe, ..self }
+    }
+
+    fn index(&self) -> (usize, usize) {
+        (self.row, self.column)
     }
 }
 
-impl Network {
-    fn connected(&mut self, direction: Direction) -> Option<Tile> {
-        match direction {
-            Direction::T => self.offset(-1, 0),
-            Direction::R => self.offset(0, 1),
-            Direction::B => self.offset(1, 0),
-            Direction::L => self.offset(0, -1),
-        }
-        .and_then(|(neighbour, row, column)| {
-            let tile = self.tiles[self.row][self.column];
-            if tile.connected(neighbour, direction) {
-                self.row = row;
-                self.column = column;
-                Some(neighbour)
-            } else {
-                None
-            }
+fn map(data: &str) -> Vec<Vec<Tile>> {
+    data.lines()
+        .enumerate()
+        .map(|(i, line)| {
+            line.chars()
+                .enumerate()
+                .map(|(j, pipe)| Tile::new(i, j, pipe))
+                .collect::<Vec<Tile>>()
         })
-    }
+        .collect()
+}
 
-    fn offset(&self, row: isize, column: isize) -> Option<(Tile, usize, usize)> {
-        let row = self.row.checked_add_signed(row);
-        let column = self.column.checked_add_signed(column);
-        if let Some((row, column)) = row.and_then(|r| column.map(|c| (r, c))) {
-            self.tiles
-                .get(row)
-                .and_then(|r| r.get(column))
-                .map(|&tile| (tile, row, column))
-        } else {
-            None
+fn origin(map: &Vec<Vec<Tile>>) -> Option<&Tile> {
+    map.iter()
+        .flat_map(|row| row.iter())
+        .filter(|tile| tile.origin)
+        .next()
+}
+
+fn neighbour(tiles: &Vec<Vec<Tile>>, tile: Tile, direction: Direction) -> (Direction, Tile) {
+    match direction {
+        Direction::T => clip(tiles, tile.index(), -1, 0),
+        Direction::R => clip(tiles, tile.index(), 0, 1),
+        Direction::B => clip(tiles, tile.index(), 1, 0),
+        Direction::L => clip(tiles, tile.index(), 0, -1),
+    }
+    .map_or_else(
+        || neighbour(tiles, tile, direction.next()),
+        |&candidate| {
+            if tile.pipe.connected(candidate.pipe, direction) {
+                (direction, candidate.connect())
+            } else {
+                neighbour(tiles, tile, direction.next())
+            }
+        },
+    )
+}
+
+fn clip(tiles: &Vec<Vec<Tile>>, index: (usize, usize), row: isize, column: isize) -> Option<&Tile> {
+    let (r, c) = index;
+    let row = r.checked_add_signed(row);
+    let column = c.checked_add_signed(column);
+    if let Some((row, column)) = row.and_then(|r| column.map(|c| (r, c))) {
+        tiles.get(row).and_then(|r| r.get(column))
+    } else {
+        None
+    }
+}
+
+fn create(input: &str) -> Result<Vec<Vec<Tile>>> {
+    let mut map = map(input);
+    if let Some(&origin) = origin(&map) {
+        let mut direction = Direction::T;
+        let mut tile = origin;
+
+        let (first, _) = neighbour(&map, tile, direction);
+        let (last, _) = neighbour(&map, tile, first.next());
+        map[tile.row][tile.column] = tile.link(first, last);
+
+        loop {
+            (direction, tile) = neighbour(&map, tile, direction.previous());
+            if tile.origin {
+                return Ok(map);
+            } else {
+                map[tile.row][tile.column] = tile;
+            }
         }
     }
+
+    bail!("could not locate origin")
 }
 
 pub fn part_one(input: &str) -> Result<usize> {
-    let mut network = Network::from(input);
-    let directions: [Direction; 4] = [Direction::T, Direction::R, Direction::B, Direction::L];
-    let mut i = 0;
-    let mut count = 0;
-    loop {
-        let direction = directions[i];
-        if let Some(tile) = network.connected(direction) {
-            count = count + 1;
-
-            if tile == Tile::ST {
-                return Ok(count / 2);
-            }
-
-            i = (i + directions.len() - 1) % directions.len();
-        } else {
-            i = (i + 1) % directions.len();
-        }
-    }
+    let map = create(input)?;
+    let network: Vec<&Tile> = map
+        .iter()
+        .flat_map(|row| row.iter())
+        .filter(|tile| tile.connected)
+        .collect();
+    Ok((network.len() + 1) / 2)
 }
 
-pub fn part_two(input: &str) -> Result<i64> {
-    Ok(1)
+pub fn part_two(input: &str) -> Result<usize> {
+    let map = create(input)?;
+    let mut count = 0;
+    for row in map {
+        let mut breached = false;
+        let mut pending = 0;
+        for tile in row {
+            if tile.connected {
+                if tile.pipe.northbound() {
+                    breached = !breached;
+                    count = count + pending;
+                    pending = 0;
+                }
+            } else if breached {
+                pending = pending + 1;
+            }
+        }
+    }
+
+    Ok(count)
 }
 
 #[cfg(test)]
@@ -152,12 +240,14 @@ mod tests {
     #[test]
     fn part_2_example() -> Result<()> {
         let input = include_str!("../res/10.example");
+        assert_eq!(part_two(input)?, 1);
         Ok(())
     }
 
     #[test]
     fn part_2_actual() -> Result<()> {
         let input = include_str!("../res/10.actual");
+        assert_eq!(part_two(input)?, 265);
         Ok(())
     }
 }
